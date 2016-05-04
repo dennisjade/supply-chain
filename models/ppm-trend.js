d468da7f9ba7be2c66f710e07898e47de96ec877
dennisjade <toribiod@sg.ibm.com>
2016-05-04 17:56:51 +0800

    add global owners

--- models/ppm-trend.js
+++ models/ppm-trend.js
@@ -5,27 +5,45 @@
       
       //connString = "DATABASE=SQLDB;HOSTNAME=75.126.155.153;PORT=50000;PROTOCOL=TCPIP;UID=user15506;PWD=SAIL2RSdn8Z3;";
 
-  module.exports.getTrends = function(partNumber, callback) {
+  module.exports.getTrends = function(partNumber, options, callback) {
     try {
 
         if (!global.dbConn.connected)
           global.dbConn = ibmdb.openSync(config.dbConnString);
 
         var currDate = new Date()
-        var month = currDate.getMonth()-1 //minus 2 because we dont have data for the current months
+        var month = currDate.getMonth() //minus 2 because we dont have data for the current months
         var year = currDate.getFullYear()
+
         var query = "select PPM_FLAG, PPM_FLAG_TEXT, TREND_TEXT from PPM_TREND where" +
                     " IBMPN='"+partNumber.toUpperCase()+"' and CAST(PERIOD_MONTH as INTEGER)="+month+" and CAST(PERIOD_YEAR as INTEGER)="+year+
                     " fetch first rows only"
         console.log('gettrend', query)
         global.dbConn.query(query, function (err, rows, docs) {
-            if (err) {
-              console.log('Error getting PPM_TREND: ', err);
-              callback(err,null)
-            } else {
-              //console.log(err, docs)
+          if (err) {
+            console.log('Error getting PPM_TREND: ', err);
+            callback(err,null)
+          } else {
+            //console.log(err, docs)
+            if (rows[0].length>0)
               callback(null, rows[0])
+            else{
+              query = "select PPM_FLAG, PPM_FLAG_TEXT, TREND_TEXT from PPM_TREND where" +
+                    " IBMPN='"+partNumber.toUpperCase()+"'" +
+                    " ORDER BY "+
+                    " CAST((REPLACE(BUILT_YEAR_MONTH,'_','-') || '-1' ) as DATE) DESC, " +
+                    " CAST((REPLACE(CALENDER_YEAR_MONTH,'_','-') || '-1' ) as DATE) DESC" + 
+                    " FETCH FIRST 1 ROWS ONLY "
+              console.log('getPPMTrends: Attempting to get the whatever is the latest data we have')
+              global.dbConn.query(query, function (err, rows2, docs) {
+                if (err) {
+                  callback(err, null)
+                }else{
+                  callback(null, rows2[0])
+                }
+              }
             }
+          }
         });
     } catch (e) {
       console.log(e.message);
